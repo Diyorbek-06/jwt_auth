5d03ad23c98e3918fb6c79e65ead1d81ded4d5c8
Diyorbek-06 <diyorbekt146@gmail.com>
2025-05-02 15:48:20 +0500

    first commit

---
+++ account/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['username', 'password']
+        model = User
+
+
+
---
+++ account/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from yaml import serialize
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+# Create your views here.
+
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data['username']
+        password = data['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return Response({"data": user.username, 'msg': "siz royxat otdingiz"})
+
+
+
+        # def post(self, request):
+    #     serializer = RegisterSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({"data": serializer.data, 'msg':"siz royxat otdingiz"})
+    #     return Response({"msg":"xatolik", 'status':400})
