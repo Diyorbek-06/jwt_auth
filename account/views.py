938c7f2bc99e5999b4318de68310434056e9cab9
Diyorbek-06 <diyorbekt146@gmail.com>
2025-05-03 10:52:43 +0500

    test qo'shildi

--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
 from yaml import serialize
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
 
 
@@ -25,3 +29,11 @@ class RegisterView(APIView):
     #         serializer.save()
     #         return Response({"data": serializer.data, 'msg':"siz royxat otdingiz"})
     #     return Response({"msg":"xatolik", 'status':400})
+
+class Test(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    def get(self, request):
+        return Response({'msg':"nimadir"})
+
+
